081e532eae045d8d69a3192ab86c5f0ae29c4005
Anamika Ranjan <97833859+anamika3012@users.noreply.github.com>
2026-01-07 20:55:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count distinct elements in every window/count-distinct-elements-in-every-window.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    vector<int> countDistinct(vector<int> &arr, int k) {
+        // code here
+        int n = arr.size();
+        unordered_map<int,int> mpp;
+        vector<int> ans;
+        
+        int i=0, j=0;
+
+        for(; j<n; j++){
+            mpp[arr[j]]++;
+            for(; i<n && (j-i+1>k); i++){
+                mpp[arr[i]]--;
+                if(mpp[arr[i]]==0) mpp.erase(arr[i]);
+            }
+            if(j-i+1==k) ans.push_back(mpp.size());
+        }
+
+        return ans;
+    }
+};
